deeb1169612def7f9246c59fa4cad3542180d483
Santiago_Diaz <santiago.diaz11d@gmail.com>
2020-05-05 11:03:45 +1000

    Fixing the problems

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+constructor(){
+  super();
+
+  this.state = {
+    texto: ''
+  }
+}
+
+handleChange = (event) => {(
+  this.setState({texto: event.target.value})
+)}
+
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.handleChange} value={this.state.texto}/>
+    <p className="repeater">{this.state.texto}</p>
       </div>
     );
   }
